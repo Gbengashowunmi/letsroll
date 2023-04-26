b87d99717159d02e73431cb6bafbb6a82ccd1518
unknown <gbengashowunmi01@gmail.com>
2023-04-26 14:24:09 +0100

    first commit

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -91,7 +91,7 @@ export default function Landing() {
           data-aos-delay="10"
           data-aos-duration="1000"
           data-aos-easing="ease-in-out"
-          // data-aos-mirror="true"
+          data-aos-mirror="true"
           data-aos-once="false"
           data-aos-anchor-placement="top-center"
         >
