646780e2d1fbf539a13d46bed56d0e44ba30347d
unknown <gbengashowunmi01@gmail.com>
2023-04-26 13:30:01 +0100

    first commit

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -86,7 +86,7 @@ export default function Landing() {
       <div className="about">
         <div
           className="about_left"
-          data-aos="slide-left"
+          data-aos="slide-right"
           data-aos-offset="-200"
           data-aos-delay="10"
           data-aos-duration="1000"
